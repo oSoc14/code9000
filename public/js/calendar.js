4988ea05f69477748be23033562337bf944dd96e
Sander Meert <satanderm@gmail.com>
2014-07-01 15:11:18 +0200

    Calendar javascript works Load events with Ajax and display them

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,40 +1,72 @@
 $(document).ready(function() {
-  // Full calendar plugin
-  $('#calendar').fullCalendar({
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
-    },
-    defaultDate: '2014-06-12',
-    editable: true,
-    events: [
-      {
-        title: 'Event1',
-        start: '2014-06-12',
-        url: 'http://www.google.be'
+
+  var _events = [];
+  getEvents();
+  function getEvents()
+  {
+    var events = [];
+    $.ajax({
+      type:"GET",
+      url: "calendar/api/events",
+      cache: false,
+      dataType: "json",
+      contentType: "application/json",
+      success:function(data){
+        setTimeout(function(){
+         events = data['school']['groups'];
+         parseEvents(events);
+        },800);
       },
-      {
-        title: 'Event2',
-        start: '2014-06-14'
+      error:function(xhr, status, errorThrown) {
+        alert(status + ', ' + errorThrown);
       }
-      // etc...
-    ],
-    theme: true,
-    themeButtonIcons: {
-      prev: 'circle-triangle-w',
-      next: 'circle-triangle-e',
-      prevYear: 'seek-prev',
-      nextYear: 'seek-next'
-    },
-    /*{
-     url: 'php/get-events.php',
-     error: function() {
-     $('#script-warning').show();
-     }
-     }*/
-    loading: function(bool) {
-      $('#loading').toggle(bool);
-    }
-  });
+    });
+  }
+
+  function parseEvents(events)
+  {
+    $.each( events, function( index, value ){
+      $.each( value['appointments'], function( i, v ){
+        var newItem = {};
+        newItem['title'] = v['title'];
+        newItem['start'] = v['start_date'];
+        newItem['end']   = v['end_date'];
+        _events.push(newItem);
+      });
+    });
+    console.log(_events);
+    renderEvents();
+  }
+
+  function renderEvents()
+  {
+    // Full calendar plugin
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      defaultDate: '2014-06-12',
+      editable: true,
+      events: _events,
+      timeFormat: 'H(:mm)',
+      theme: true,
+      themeButtonIcons: {
+        prev: 'circle-triangle-w',
+        next: 'circle-triangle-e',
+        prevYear: 'seek-prev',
+        nextYear: 'seek-next'
+      },
+      /*{
+       url: 'php/get-events.php',
+       error: function() {
+       $('#script-warning').show();
+       }
+       }*/
+      loading: function(bool) {
+        $('#loading').toggle(bool);
+      }
+    });
+  }
 });
