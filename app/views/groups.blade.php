58ead5bf9c35adfc6597391e61548dcb23962fe5
Nick Denys <nickdenys@gmail.com>
2014-07-01 13:08:01 +0200

    Predefined properties

--- app/views/groups.blade.php
+++ app/views/groups.blade.php
@@ -10,20 +10,20 @@
 <ul class="modules-overview">
   <li class="module row">
     <div class="module-detail-container col-xs-12 col-sm-6">
-      <span>School 1</span>
-      <span class="pull-right">Location</span>
+      <span>Group 1</span>
+      <span class="pull-right"># users</span>
     </div>
   </li>
   <li class="module row">
     <div class="module-detail-container col-xs-12 col-sm-6">
-      <span>School 2</span>
-      <span class="pull-right">Location</span>
+      <span>Group 2</span>
+      <span class="pull-right"># users</span>
     </div>
   </li>
   <li class="module row">
     <div class="module-detail-container col-xs-12 col-sm-6">
-      <span>School 3</span>
-      <span class="pull-right">Location</span>
+      <span>Group 3</span>
+      <span class="pull-right"># users</span>
     </div>
   </li>
 </ul>
--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -10,20 +10,20 @@
 <ul class="modules-overview">
   <li class="module row">
     <div class="module-detail-container col-xs-12 col-sm-6">
-      <span>School 1</span>
-      <span class="pull-right">Location</span>
+      <span>User 1</span>
+      <span class="pull-right">Role</span>
     </div>
   </li>
   <li class="module row">
     <div class="module-detail-container col-xs-12 col-sm-6">
-      <span>School 2</span>
-      <span class="pull-right">Location</span>
+      <span>User 2</span>
+      <span class="pull-right">Role</span>
     </div>
   </li>
   <li class="module row">
     <div class="module-detail-container col-xs-12 col-sm-6">
-      <span>School 3</span>
-      <span class="pull-right">Location</span>
+      <span>User 3</span>
+      <span class="pull-right">Role</span>
     </div>
   </li>
 </ul>
