bc21f0d1bea8ccf056ced1624cee654d5931928f
Van Acker Bjorn <bjorvack@gmail.com>
2014-07-02 15:36:12 +0200

    #4 Added DateTimepicker http://tarruda.github.io/bootstrap-datetimepicker/

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -7,7 +7,7 @@
     {{ HTML::style("css/bootstrap.min.css") }}
     {{ HTML::style("css/bootstrap-theme.min.css") }}
     <!-- Bootstrap Datepicker -->
-    {{ HTML::style("css/datepicker.css") }}
+    {{ HTML::style("css/datetimepicker.css") }}
     <!-- Global styles for this template -->
     @yield('header')
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -30,7 +30,7 @@
 @yield('content')
 {{ HTML::script("js/jquery-1.11.1.min.js") }}
 {{ HTML::script("js/bootstrap.min.js") }}
-{{ HTML::script("js/bootstrap-datepicker.js") }}
+{{ HTML::script("js/bootstrap-datetimepicker.js") }}
 @yield('footerScript')
 </body>
 </html>
