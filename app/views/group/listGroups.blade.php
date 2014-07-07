466cd0e85672aeade7069d68afe063ed4c165650
Nick Denys <nickdenys@gmail.com>
2014-07-07 17:13:21 +0200

    #12 Better structure added to subpages of groups/users/schools

--- app/views/group/listGroups.blade.php
+++ app/views/group/listGroups.blade.php
@@ -5,49 +5,56 @@
 @stop
 
 @section('content')
-<h1>
-    {{ $schoolName }}
-</h1>
-<table id="groupTable" class="display" cellspacing="0" width="100%">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>iCal</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
+<div class="row">
+  <div class="col-xs-12">
+    <ol class="breadcrumb">
+      <li><a href="{{ route('landing') }}">Home</a></li>
+      <li class="active">Groups</li>
+    </ol>
+  </div>
+</div>
+<h1>Groups</h1>
+<div class="row">
+  <div class="col-xs-12 table-responsive">
+    <table id="groupTable" class="table table-striped" cellspacing="0" width="100%">
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>iCal</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
 
-    <tfoot>
-        <tr>
-            <th>Name</th>
-            <th>iCal</th>
-            <th>Actions</th>
-        </tr>
-    </tfoot>
+      <tbody>
+      @foreach($groups as $group)
+      <tr>
+        <td>{{ $group->name }}</td>
+        @if($group->school)
+        <td><a href='./export/{{$group->school->short}}/{{str_replace($group->school->short."_","",$group->name)}}/ical.ics'>/export/{{$group->school->short}}/{{str_replace($group->school->short."_","",$group->name)}}/ical.ics</a></td>
+        @else
+        <td>NO EXPORT</td>
+        @endif
+        <td>
+          <a href="{{route('group.edit',$group->id)}}"><span class="glyphicon glyphicon-pencil"></span></a>&nbsp;
+          <a href="#"><span class="glyphicon glyphicon-remove-sign"></span></a>
+        </td>
+      </tr>
+      @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
 
-    <tbody>
-        @foreach($groups as $group)
-            <tr>
-                <td>{{ $group->name }}</td>
-                @if($group->school)
-                <td><a href='./export/{{$group->school->short}}/{{str_replace($group->school->short."_","",$group->name)}}/ical.ics'>/export/{{$group->school->short}}/{{str_replace($group->school->short."_","",$group->name)}}/ical.ics</a></td>
-                @else
-                <td>NO EXPORT</td>
-                @endif
-                <td>
-                    <a href="#"><span class="glyphicon glyphicon-remove-sign"></span></a>
-                    <a href="{{route('group.edit',$group->id)}}"><span class="glyphicon glyphicon-pencil"></span></a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
 @stop
 
 @section('footerScript')
-{{ HTML::script('js/app.js') }}
+
 {{ HTML::script('packages/datatables/js/jquery.dataTables.min.js') }}
-{{ HTML::style('packages/datatables/css/jquery.dataTables.min.css') }}
+
+{{ HTML::script('//cdn.datatables.net/plug-ins/be7019ee387/integration/bootstrap/3/dataTables.bootstrap.js') }}
+{{ HTML::style('//cdn.datatables.net/plug-ins/be7019ee387/integration/bootstrap/3/dataTables.bootstrap.css') }}
+
+{{ HTML::script('js/app.js') }}
 <script>
     $(document).ready(function() {
         $('#groupTable').dataTable();
--- app/views/school/index.blade.php
+++ app/views/school/index.blade.php
@@ -5,38 +5,58 @@
 @stop
 
 @section('content')
-<h1>Schools</h1>
-<table class="table table-striped">
-    <tr>
-        <th>
-            Name
-        </th>
-        <th>
-            Short name
-        </th>
-        <th>
-            # Groups
-        </th>
-        <th>
-            Actions
-        </th>
-    </tr>
-    @foreach($schools as $school)
-    <tr>
+<div class="row">
+  <div class="col-xs-12">
+    <ol class="breadcrumb">
+      <li><a href="{{ route('landing') }}">Home</a></li>
+      <li class="active">Schools</li>
+    </ol>
+  </div>
+</div>
+<div class="row">
+  <div class="col-xs-12 table-responsive">
+    <h1>Schools</h1>
+    <table id="groupTable" class="table table-striped" cellspacing="0" width="100%">
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Short Name</th>
+        <th># Groups</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
+      <tbody>
+      @foreach($schools as $school)
+      <tr>
         <td>{{ HTML::linkRoute('school.detail', $school->name, ['id' => $school->id], []) }}</td>
         <td>{{$school->short}}</td>
         <td>{{count($school->groups)}}</td>
         <td>
-            <a href="{{route('school.detail',$school->id)}}"><span class="glyphicon glyphicon-eye-open"></span></a>
-            <a href="#"><span class="glyphicon glyphicon-pencil"></span></a>
-            <a href="#"><span class="glyphicon glyphicon-trash"></span></a>
+          <a href="#" title="Edit"><span class="glyphicon glyphicon-pencil"></span></a>
+          <a href="#" title="Remove"><span class="glyphicon glyphicon-trash"></span></a>
         </td>
-    </tr>
-    @endforeach
-</table>
-
+      </tr>
+      @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
 @stop
 
 @section('footerScript')
+
+
+{{ HTML::script('packages/datatables/js/jquery.dataTables.min.js') }}
+
+{{ HTML::script('//cdn.datatables.net/plug-ins/be7019ee387/integration/bootstrap/3/dataTables.bootstrap.js') }}
+{{ HTML::style('//cdn.datatables.net/plug-ins/be7019ee387/integration/bootstrap/3/dataTables.bootstrap.css') }}
+
 {{ HTML::script('js/app.js') }}
+
+<script>
+  $(document).ready(function() {
+    $('#groupTable').dataTable();
+  } );
+</script>
 @stop
+
