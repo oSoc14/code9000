6c33e584af571c1db370eea298a4e2b13fbc094a
Van Acker Bjorn <bjorvack@gmail.com>
2014-07-10 11:16:07 +0200

    Form refill on errors

--- app/views/user/edit.blade.php
+++ app/views/user/edit.blade.php
@@ -15,17 +15,16 @@
     @endforeach
 
     <div class="form-group">
-        <label for="name">First name</label>
-        <input type="text" class="form-control" id="name" name="name" value="{{$user->first_name}}" placeholder="{{$user->first_name}}">
+        {{Form::label('name', 'First name')}}
+        {{Form::text('name', $user->first_name , ['class'=>'form-control', 'placeholder'=>$user->first_name])}}
     </div>
     <div class="form-group">
-        <label for="surname">Last name</label>
-        <input type="text" class="form-control" id="surname" name="surname" value="{{$user->last_name}}" placeholder="{{$user->last_name}}">
-
+        {{Form::label('surname', 'Last name')}}
+        {{Form::text('surname', $user->last_name , ['class'=>'form-control', 'placeholder'=>$user->last_name])}}
     </div>
     <div class="form-group">
-        <label for="email">E-mail</label>
-        <input type="email" class="form-control" id="email" name="email" value="{{$user->email}}" placeholder="{{$user->email}}">
+        {{Form::label('email', 'E-mail')}}
+        {{Form::email('email', $user->email , ['class'=>'form-control', 'placeholder'=>$user->email])}}
     </div>
     <div class="form-group">
         <label for="password">New Password</label>
