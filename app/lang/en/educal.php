cb6a8954ae42f76fe949a88edcc1c671b1ceaef5
Bjorn Van Acker <bjorvack@gmail.com>
2014-07-21 16:07:46 +0200

    #28 Translations

--- app/lang/en/educal.php
+++ app/lang/en/educal.php
@@ -11,7 +11,34 @@ return array(
     |
     */
 
-    //Menu
+    'lead' => 'EduCal is an application for schools to easily create and manage calendars that can be shared with parents.',
+    'login' => 'Login',
+    'loginaccount' => 'Log in to your account',
+    'register' => 'Register',
+    'or' => 'or',
+    'getstarted' => 'get started',
+    'contact' => 'contact',
+    'errors' => 'Errors',
+    'short' => 'shortcode',
+    'startdate' => 'startdate',
+    'enddate' => 'enddate',
+
+    'delete' => 'delete',
+    'cancel' => 'cancel',
+    'confirmation' => 'confirmation',
+    'confirmationmsg' => 'Are you sure you want to delete this item?',
+
+    'email' => 'Email address',
+    'password' => 'password',
+    'newpassword' => 'new password',
+    'repeatpassword' => 'repeat password',
+    'remember' => 'Remember me',
+    'registeruser' => 'Register as a user',
+    'registerschool' => 'Warning! If you want to register as a school, click <a href="#" id="showSchoolRegisterModal">here</a>.',
+    'registeraschool' => 'Register as a school',
+    'terms' => ' I agree to the <a href="#">terms of service</a>.',
+    'city' => 'city',
+
     'calendar' => 'calendar',
     'schools' => 'schools',
     'school' => 'school',
@@ -20,6 +47,7 @@ return array(
     'groups' => 'groups',
     'group' => 'group',
     'about' => 'about',
+    'language' => 'language',
     'settings' => 'settings',
     'logout' => 'logout',
 
@@ -29,31 +57,60 @@ return array(
     'month' => 'month',
     'week' => 'week',
     'day' => 'day',
+    'years' => 'years',
+    'months' => 'months',
+    'weeks' => 'weeks',
+    'days' => 'days',
     'loading' => 'loading ...',
 
-    'eventdetails' => 'event details',
+    'addgroup' => 'add group',
+    'whichschool'=>'For which school?',
+    'permissions' => 'permissions',
+    'managegroups' => 'can create groups',
+    'manageusers' => 'can add users',
+    'manageevents' => 'can add,edit and remove events',
+
+    'eventdetails' => 'details',
     'createevent' => 'create event',
     'editevent' => 'edit event',
     'deleteevent' => 'delete event',
-    'export' => 'export to calendar',
+    'export' => 'export calendar',
     'starts' => 'starts',
     'ends' => 'ends',
 
     'title' => 'title',
     'description' => 'description',
-    'allday' => 'all day',
-    'repeatingevent' => 'Repeating event',
-    'recurrence' => 'Recurrence (every x period until date)',
+    'allday' => 'al day',
+    'repeatingevent' => 'repeating event',
+    'every' => 'every',
+    'until' => 'util',
+    'recurrence' => 'Repeat (every x period until enddate)',
     'actions' => 'actions',
     'name' => 'name',
-    'surname' => 'surname',
+    'surname' => 'sur naam',
+
 
     'editgroup' => 'edit group',
     'updategroup' => 'update group',
     'groupinfo' => 'group info',
+    'groupname' => 'groep name',
+    'nousers'=> 'No users found to be addedGeen gebruikers die kunnen toegevoegd worden',
     'adduser' => 'add user',
-    'nousers' => 'There are no users to be added',
-    'usersingroup' => 'Users in this group'
+    'usersingroup' => 'users in this group',
+    'creategroup' => 'create and add group',
+    'noschools' => 'There are no schools to add a group to.',
+    'nameunchangeable' => 'This name can not be changed',
+    'urls' => 'Urls',
+    'noexport' => 'NO EXPORT',
 
+    'editschool' => 'edit school',
+
+    'editinformation' => 'edit information',
+    'backto' => 'back to :page',
+    'savechanges' => 'save changes',
+    'activated' => 'activated',
+
+    '#ofusers' => '# of users',
+    '#ofgroups' => '# of groups',
 
 );
