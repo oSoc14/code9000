c3d37a80618745dc48b474de3157bc174c70021d
Nick Denys <nickdenys@gmail.com>
2014-07-25 13:33:07 +0200

    Database.php fix

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'educal',
-			'username'  => 'root',
-			'password'  => 'bitnami',
+			'host'      => 'forge',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
