882360f8e64b9632d41d6f1c1425417ef2413c90
Sander Meert <satanderm@gmail.com>
2014-07-25 13:07:57 +0200

    Finished translating help page

--- app/views/help.blade.php
+++ app/views/help.blade.php
@@ -12,7 +12,7 @@
       <h2>Pagina's</h2>
       <ul>
         <li>
-          <h3>Calendar</h3>
+          <h3>Kalendar</h3>
           <p>De kalender is eigenlijk de centrale hub van EduCal. Alle events binnenin je groepen en school zijn hier terug te vinden. Je kan ook nieuwe events aanmaken en toevoegen tot de kalender (als je daar gerechtigd voor bent). De kalender op zich kan in drie verschillende modussen bekeken worden: maandelijks, wekelijks of dagelijks.</p>
           <p>Wanneer je op een event klikt verschijnt er een apart venster dat alle details over dat event toont, zoals de omschrijving en de begin- en einddatum. Onderaan dat venster vind je ook opties terug om dat evenement te exporteren, z'n details te wijzigen of om het te verwijderen uit de kalender, afhankelijk van je groepsrechten.</p>
         </li>
@@ -42,6 +42,44 @@
         <li>
           <h3>Gebruikers</h3>
           <p>Hier vind je een overzicht van alle gebruikers binnen uw school. Je kunt ze activeren (of deactiveren), hun details wijzigen of ze verwijderen uit uw school. Je kunt snel alle namen en emailadressen terugvinden. Net als de andere pagina's vind je alles terug in een handige tabel met sorteermogelijkheden en een zoekfunctie.</p>
+        </li>
+        <li>
+            <h3>Instellingen</h3>
+            <p>Als gebruiker heb je enkele instellingen die je kan aanpassen. Deze instellingen zijn:
+                <ul>
+                    <li>Familienaam</li>
+                    <li>Naam</li>
+                    <li>E-mail adres</li>
+                    <li>Taal</li>
+                    <li>Paswoord aanpassen</li>
+                </ul>
+            </p>
+
+            <h4><u>Taal wijzigen</u></h4>
+            <p>EduCal is nu beschikbaar in 3 verschillende talen: Engles, Nederlands and Frans.</p>
+        </li>
+        <li>
+            <h3>Feedback</h3>
+            <p>Wil je bijdragen aan de vooruitgang van EduCal? Heb je een goed idee dat je wil delen? Contacteer ons dan, we stellen feedback ten zeerste op prijs. :)</p>
+        </li>
+        <li>
+            <h3>FAQ</h3>
+            <p><b>Ik heb me net geregistreerd maar ik kan niet inloggen. Waarom niet?</b></p>
+            <p>Als je je eerst registreert bij een school, dan moet iemand van de schooladministratie uw account activeren. Dit is nodig voor veiligheidsredenen.</p>
+            <p><b>Mijn organisatie is geen school. Kan ik dit platform dan nog steeds gebruiken?</b></p>
+            <p>Natuurlijk! EduCal kan gebruikt worden door elke organisatie die er baat bij heeft van een centrale kalender te hebben die ze gemakkelijk kunnen delen.</p>
+            <p><b>Wat is het verschil tussen scholen en groepen?</b></p>
+            <p>Scholen zijn de organisaties die zich op het EduCal platform registreren. Elke school heeft dan verschillende groepen waar ze gebruikers aan kunnen toekennen. De kalenders worden gegenereerd op basis van de groepen, niet op basis van de school.</p>
+            <p><b>Kan ik uitzonderingen toevoegen voor herhalende events?</b></p>
+            <p>Helaas niet. Dit is een feature die hoog op de ToDo-lijst staat, maar we waren niet in staat om dit af te leveren met deze versie. Voorlopig zal je dus manueel rekening moeten houden met uitzonderingen.</p>
+            <p><b>Kan een gebruiker in meer dan 1 school zitten tegelijk?</b></p>
+            <p>Nee. Deze feature staat ook hoog op de ToDo-lijst, maar is ook nog niet geïmplementeerd in deze versie.</p>
+            <p><b>Zal de iCal link alle events van de school inladen?</b></p>
+            <p>Neen! De exports zijn gebaseerd op groepen. Dus je laadt enkel de events in van de groep waarvan je de link hebt, alsook de globale events die tellen voor alle groepen. Ook zal je enkel events binnenhalen van 1 jaar terug t.e.m. 1 jaar in de toekomst.</p>
+            <p><b>Moet ik me inloggen of registreren om de iCal of PDF files te kunnen downloaden?</b></p>
+            <p>Neen. Zolang je de link hebt, kan je deze bestanden op eender welk moment raadplegen.</p>
+            <p><b>EduCal is net gecrasht, hoe komt dit?</b></p>
+            <p>EduCal is zeer recent gemaakt en er is nog veel ruimte voor verbetering, alsook zijn er waarschijnlijk nog enkele bugs die zich verschuilen hier en daar. We hopen op feedback als je ervaring niet optimaal was zodat we verbeteringen kunnen uitvoeren. Je kan ons contacteren met de feedback link in het linker menu.</p>
         </li>
       </ul>
     </div>
