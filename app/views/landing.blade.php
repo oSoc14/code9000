72c303c9bdd1ce894fae4bacca398d1ecf33d95a
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2014-10-06 13:25:03 +0200

    Added close button to the email form.

--- app/views/landing.blade.php
+++ app/views/landing.blade.php
@@ -143,6 +143,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
           <h4>Geef uw email adres in</h4>
         </div>
         <div class="modal-body">
