bc1528762d24599fbeb72ca44cd4e0f563751ac1
Thomas <hello@thomasg.be>
2015-07-08 12:08:37 +0200

    Use grunt to build sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,17 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    sass: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'public/css/calendar.css': 'public/sass/calendar.scss'
+            }
+        }
+    },
+
     concat: {
       // 2. Configuration for concatinating files goes here.
       dist: {
@@ -29,8 +40,9 @@ module.exports = function(grunt) {
   // 3. Where we tell Grunt we plan to use this plug-in.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
 
   // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'sass']);
 
 };
