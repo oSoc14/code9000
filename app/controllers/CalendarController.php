9d83e41325b76a442350410d8113ee3071eae25c
Bertware <bert.marcelis@outlook.com>
2015-07-10 11:38:22 +0200

    Finished refactoring CalendarController,  #4

--- app/controllers/CalendarController.php
+++ app/controllers/CalendarController.php
@@ -8,44 +8,44 @@ class CalendarController extends \BaseController
 {
 
     /**
-     * Display a listing of the groups.
+     * Display a listing of the calendars.
      *
      * @return Response
      */
     public function index()
     {
-        if (Sentry::check()) {
-            // Find active user and set default variables to null
-            $user       = Sentry::getUser();
-            $calendars = null;
-
-            // Check if user is superAdmin
-            if ($user->hasAccess('superadmin')) {
-                $calendars = Calendar::where('school_id', '<>', '')->get();
-                $calendars = $calendars->load('school');
-
-                // Return view with selected parameters
-                return View::make('calendarManagement.listGroups')->with('groups', $calendars);
-
-            } elseif ($user->hasAccess('calendar')) {
-
-                // Get school_id, by which we will search for related groups
-                $schoolId = $user->school_id;
-
-                // Find all groups with certain school_id
-                $calendars = Calendar::where('school_id', '=', $schoolId)->get();
-                $calendars = $calendars->load('school');
-
-                // Return view with selected parameters
-                return View::make('calendarManagement.listGroups')->with('groups', $calendars);
-
-            } else {
-                // If no permissions, redirect the user to the calendar index page
-                return Redirect::route('calendar.index');
-            }
-        } else {
+        if (!Sentry::check()) {
             return Redirect::route('landing');
         }
+        // Find active user and set default variables to null
+        $user = Sentry::getUser();
+        $calendars = null;
+
+        // Check if user is superAdmin
+        if ($user->hasAccess('superadmin')) {
+            $calendars = Calendar::where('school_id', '<>', '')->get();
+            $calendars = $calendars->load('school');
+
+            // Return view with selected parameters
+            return View::make('calendarManagement.listGroups')->with('groups', $calendars);
+
+        } elseif ($user->hasAccess('editor')) {
+
+            // Get school_id, by which we will search for related groups
+            $schoolId = $user->school_id;
+
+            // Find all groups with certain school_id
+            $calendars = Calendar::where('school_id', '=', $schoolId)->get();
+            $calendars = $calendars->load('school');
+
+            // Return view with selected parameters
+            return View::make('calendarManagement.listGroups')->with('groups', $calendars);
+
+        } else {
+            // If no permissions, redirect the user to the calendar index page
+            return Redirect::route('calendar.index');
+        }
+
 
     }
 
@@ -59,31 +59,31 @@ class CalendarController extends \BaseController
     // TODO: Add colors/codes to calendars
     public function create()
     {
-        if (Sentry::check()) {
-            $schools = null;
-            // Find active user
-            $user = Sentry::getUser();
-
-            // If user is a superAdmin (has access to school), show school-dropdown for the view where the user can
-            // choose which school he wants to add the calendar to
-            if ($user->hasAccess('superadmin')) {
-                $schools = School::lists('name', 'id');
-
-                return View::make('calendarManagement.createGroup')->with('schools', $schools);
-
-            } else {
-
-                if ($user->hasAccess('calendar')) {
-                    return View::make('calendarManagement.createGroup')->with('schools', null);
-                } else {
-                    // If no permissions, redirect the user to the calendar index page
-                    return Redirect::route('calendar.index');
-                }
-            }
-        } else {
+        if (!Sentry::check()) {
             // If no permissions, redirect to calendar index
             return Redirect::route('landing');
         }
+        $schools = null;
+        // Find active user
+        $user = Sentry::getUser();
+
+        // If user is a superAdmin (has access to school), show school-dropdown for the view where the user can
+        // choose which school he wants to add the calendar to
+        if ($user->hasAccess('superadmin')) {
+            $schools = School::lists('name', 'id');
+
+            return View::make('calendarManagement.createGroup')->with('schools', $schools);
+
+        } else {
+
+            if ($user->hasAccess('calendar')) {
+                return View::make('calendarManagement.createGroup')->with('schools', null);
+            } else {
+                // If no permissions, redirect the user to the calendar index page
+                return Redirect::route('calendar.index');
+            }
+        }
+
     }
 
 
@@ -94,47 +94,46 @@ class CalendarController extends \BaseController
      */
     public function store()
     {
-        if (Sentry::check()) {
-            // Find active user
-            $user = Sentry::getUser();
-
-            if ($user->hasAnyAccess(['school', 'calendar'])) {
-                $school = null;
-
-                // If user is a superAdmin (has access to school), get info from the "Input::get('school')"-field,
-                // otherwise, use the school_id from the user
-                if ($user->hasAccess('superadmin')) {
-                    $school = School::find(Input::get('school'));
-                } else {
-                    $school = $user->school;
-                }
-
-                // Generate the full groupname (schoolshort_groupshort)
-                $calendarsFullName = preg_replace('/[^A-Za-z0-9\-_ ]/', '', Input::get('name'));
-
-                // Validate input fields
-                $validator = Validator::make(
-                    [
-                        'name'        => e(Input::get('name')),
-                        'school'      => Input::get('school'),
-                    ],
-                    [
-                        'name'   => 'required',
-                        'school' => 'integer'
-                    ]
-                );
-
-                // TODO: implement calendar store
-
-                    return Redirect::route('calendarManagement.index');
-
-            } else {
-                // If no permissions, redirect the user to the calendar index page
-                return Redirect::route('calendar.index');
-            }
-        } else {
+        if (!Sentry::check()) {
             // If no permissions, redirect to calendar index
             return Redirect::route('landing');
+        }
+        // Find active user
+        $user = Sentry::getUser();
+
+        if ($user->hasAnyAccess(['school', 'calendar'])) {
+            $school = null;
+
+            // Validate input fields
+            $validator = Validator::make(
+                [
+                    'name' => e(Input::get('name')),
+                    'school' => Input::get('school'),
+                ],
+                [
+                    'name' => 'required',
+                    'school' => 'integer'
+                ]
+            );
+
+            if ($validator->fails()) {
+                return Redirect::route('group.create')->withInput()->withErrors($validator);
+            }
+
+            $calendar = new Calendar();
+            $calendar->name = e(Input::get('name'));
+            $calendar->description = e(Input::get('name'));
+            $calendar->school_id = Input::get('school');
+
+            $calendar->save();
+
+            $user->calendars()->attach($calendar);
+
+            return Redirect::route('calendarManagement.index');
+
+        } else {
+            // If no permissions, redirect the user to the calendar index page
+            return Redirect::route('calendar.index');
         }
     }
 
@@ -146,59 +145,60 @@ class CalendarController extends \BaseController
      */
     public function edit($id)
     {
-        if (Sentry::check()) {
-            // Find active user
-            $user = Sentry::getUser();
-            $calendar = Calendar::find($id);
-            // Permissions check
-            if (($user->hasAccess('admin') && $user->school_id == $calendar->school_id) || $user->hasAccess('superadmin')) {
-
-                // Find all users in the selected group
-                $users = Calendar::find($id)->users();
-                // Find all users by school
-                $schoolUsers = User::where('users.school_id', $calendar->school_id)->get();
-
-                // Find all possible users that aren't in the group yet
-                // This array of users will be used to generate a dropdown menu
-                $possibleUsers = [];
-                foreach ($schoolUsers as $su) {
-                    $found = false;
-                    foreach ($users as $u) {
-                        if ($u->id === $su->id) {
-                            $found = true;
-                            break;
-                        }
-                    }
-                    if (!$found) {
-                        array_push($possibleUsers, $su);
-                    }
-                }
-                // TODO: If users > 10 , do like this, else, get list with checkboxes if possible
-                // Transform array into usable list for dropdownmenu
-                $smartUsers = [];
-                foreach ($possibleUsers as $pus) {
-                    $smartUsers[$pus->id] = $pus->email;
-                }
-
-                // Return view with selected parameters
-                return View::make('calendarManagement.editGroups')
-                    ->with('users', $users)
-                    ->with('group', $calendar)
-                    ->with('smartUsers', $smartUsers);
-
-            } else {
-                // If no permissions, redirect the user to the calendar index page
-                return Redirect::route('calendar.index');
-            }
-        } else {
+        if (!Sentry::check()) {
             // If no permissions, redirect to calendar index
             return Redirect::route('landing');
         }
+
+        // Find active user
+        $user = Sentry::getUser();
+        $calendar = Calendar::find($id);
+
+        // Permissions check
+        if (!($user->hasAccess('admin') && $user->school_id == $calendar->school_id) && !$user->hasAccess('superadmin')) {
+            // If no permissions, redirect the user to the calendar index page
+            return Redirect::route('calendar.index');
+        }
+
+        // Find all users in the selected group
+        $users = Calendar::find($id)->users();
+        // Find all users by school
+        $schoolUsers = User::where('users.school_id', $calendar->school_id)->get();
+
+        // Find all possible users that aren't in the group yet
+        // This array of users will be used to generate a dropdown menu
+        $possibleUsers = [];
+        foreach ($schoolUsers as $su) {
+            $found = false;
+            foreach ($users as $u) {
+                if ($u->id === $su->id) {
+                    $found = true;
+                    break;
+                }
+            }
+            if (!$found) {
+                array_push($possibleUsers, $su);
+            }
+        }
+        // TODO: If users > 10 , do like this, else, get list with checkboxes if possible
+        // Transform array into usable list for dropdownmenu
+        $smartUsers = [];
+        foreach ($possibleUsers as $pus) {
+            $smartUsers[$pus->id] = $pus->email;
+        }
+
+        // Return view with selected parameters
+        return View::make('calendarManagement.editGroups')
+            ->with('users', $users)
+            ->with('group', $calendar)
+            ->with('smartUsers', $smartUsers);
+
+
     }
 
 
     /**
-     * Update the specified group in storage.
+     * Update the specified calendar in storage.
      *
      * @param  int $id
      * @return Response
@@ -206,102 +206,85 @@ class CalendarController extends \BaseController
     public function update($id)
     {
 
-        if (Sentry::check()) {
-            // Find active user and group information
-            $user  = Sentry::getUser();
-            $calendar = Calendar::find($id);
-
-            // Permission checks
-            if ($user->hasAccess('superadmin') || ($user->hasAccess('admin') && $user->school_id == $calendar->school_id)) {
-
-                // If permissions are met, get school info
-                $school = $calendar->school;
-
-
-                // Generate full calendar name
-                if(Input::get('name') != null) {
-                    $calName = preg_replace('/[^A-Za-z0-9\-_ ]/', '', Input::get('name'));
-                } else {
-                    $calName = $calendar->name;
-                }
-
-                // Make a validator to see if the new group name is unique if it's not the same as before
-                // Validate input fields
-                $validator = Validator::make(
-                    [
-                        'name' => e(Input::get('name')),
-                        'school' => Input::get('school'),
-                        'permissions' => Input::get('permissions')
-                    ],
-                    [
-                        'school' => 'integer'
-                    ]
-                );
-
-                // Error handling
-                if ($validator->fails()) {
-
-                    return Redirect::route('calendarManagement.edit', $id)->withInput()->withErrors($validator);
-
-                } elseif ($calName == $school->name || $calName == 'Administratie') {
-                    // Do not allow default groups to be renamed
-                    return Redirect::route('calendarManagement.edit', $id);
-
-                } else {
-                    /*
-                   // Set default permissions (have to be set to 0 otherwise we can't reset them if needed with Sentry
-                   $permissions    = ["event" => 0, "user" => 0, "group" => 0, "school" => 0];
-                   $permissionlist = Input::get('permissions');
-
-                  Code no longer relevant, this should go to a role settings view //todo: move to role settings
-                   // Loop through permission checkboxes from input, and put them in key)value pairs which are to be
-                   // inserted in the database
-                   if (isset($permissionlist)) {
-                       foreach ($permissionlist as $key => $value) {
-                           if ($key != "school") {
-                               $permissions[$key] = 1;
-                           }
-                       }
-                       $group->permissions = $permissions;
-                   }
-                   */
-
-                    $calendar->name = $calName;
-                    // Save/update the calendar
-                    $calendar->save();
-
-                    return Redirect::route('calendarManagement.edit', $id);
-                }
-            } else {
-                // If no permissions, redirect the user to the calendar index page
-                return Redirect::route('calendar.index');
-            }
-        } else {
+        if (!Sentry::check()) {
             // If no permissions, redirect to calendar index
             return Redirect::route('landing');
         }
+
+        // Find active user and group information
+        $user = Sentry::getUser();
+        $calendar = Calendar::find($id);
+
+        // Permission checks
+        if (!$user->hasAccess('superadmin') && !($user->hasAccess('admin') && $user->school_id == $calendar->school_id)) {
+            // If no permissions, redirect the user to the calendar index page
+            return Redirect::route('calendar.index');
+        }
+        // If permissions are met, get school info
+        $school = $calendar->school;
+
+        // Generate full calendar name
+        if (Input::get('name') != null) {
+            $calName = preg_replace('/[^A-Za-z0-9\-_ ]/', '', Input::get('name'));
+        } else {
+            $calName = $calendar->name;
+        }
+
+        // Make a validator to see if the new group name is unique if it's not the same as before
+        // Validate input fields
+        $validator = Validator::make(
+            [
+                'name' => e($calName),
+                'school' => Input::get('school'),
+                'permissions' => Input::get('permissions')
+            ],
+            [
+                'school' => 'integer'
+            ]
+        );
+
+        // Error handling
+        if ($validator->fails()) {
+
+            return Redirect::route('calendarManagement.edit', $id)->withInput()->withErrors($validator);
+
+            // TODO: take a look at "protected" calendars
+        } elseif ($calName == $school->name || $calName == 'Administratie') {
+            // Do not allow default groups to be renamed
+            return Redirect::route('calendarManagement.edit', $id);
+
+        } else {
+
+            $calendar->name = $calName;
+            // Save/update the calendar
+            $calendar->save();
+
+            return Redirect::route('calendarManagement.edit', $id);
+        }
+
+
     }
 
-
     /**
-     * Remove the specified group from storage.
+     * Remove the specified calendar from storage.
      *
      * @param  int $id
      * @return Response
      */
     public function destroy($id)
     {
-        if (Sentry::check()) {
-            // Find active user and group information
-            $user  = Sentry::getUser();
-            $calendar = Calendar::find($id);
-
-            // Permission checks
-            if ($user->hasAccess('superadmin') || ($user->hasAccess('admin') && $user->school_id == $calendar->school_id)) {
-                $calendar->delete();
-            }
-
+        if (!Sentry::check()) {
+            return;
         }
+        // Find active user and group information
+        $user = Sentry::getUser();
+        $calendar = Calendar::find($id);
+
+        // Permission checks
+        if ($user->hasAccess('superadmin') || ($user->hasAccess('admin') && $user->school_id == $calendar->school_id)) {
+            $calendar->delete();
+        }
+
     }
 
 
