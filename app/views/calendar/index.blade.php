5266f688050382c712c6b9586d46d831886ab12e
Thomas <hello@thomasg.be>
2015-07-15 09:07:41 +0200

    Refactor calendar.js

--- app/views/calendar/index.blade.php
+++ app/views/calendar/index.blade.php
@@ -3,7 +3,6 @@
 @section('header')
 {{ HTML::style("bower_components/fullcalendar/dist/fullcalendar.min.css") }}
 {{ HTML::style("bower_components/datetimepicker/jquery.datetimepicker.css") }}
-{{ HTML::style("css/calendar.css") }}
 <script type="text/javascript">
     var org = {{ $school; }};
     var user = {{ $user; }};
@@ -105,12 +104,12 @@
   <div class="divider-btm dtpicker">
     <label for="d1">Tijd</label>
     <div class="dtpicker-dt dtpicker-start">
-      <input class="input" type="date" placeholder="date">
-      <input class="input" type="time" placeholder="time">
+      <input class="input input-date d1" type="text" placeholder="date">
+      <input class="input input-time t1" type="text" placeholder="time">
     </div>
     <div class="dtpicker-dt dtpicker-end">
-      <input class="input" type="date" placeholder="date">
-      <input class="input" type="time" placeholder="time">
+      <input class="input input-date d2" type="text" placeholder="date">
+      <input class="input input-time t2" type="text" placeholder="time">
     </div>
   </div>
   <p class="divider-btm">
@@ -158,7 +157,11 @@
   {{ HTML::script("bower_components/fullcalendar/dist/lang/fr.js") }}
 @elseif(Session::get('lang') == 'de')
   {{ HTML::script("bower_components/fullcalendar/dist/lang/de.js") }}
-@endif {{ HTML::script('js/calendar.js') }}
+@endif
+
+{{ HTML::script('js/render.js') }}
+{{ HTML::script('js/editor.js') }}
+{{ HTML::script('js/calendar.js') }}
 
 {{ HTML::script('js/app.js') }}
 @stop
