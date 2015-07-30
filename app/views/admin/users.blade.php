b7e81b2996f254b64bf5ae0608d409a0939851b3
Thomas <hello@thomasg.be>
2015-07-30 09:47:26 +0200

    Start working on user_calendar modal

--- app/views/admin/users.blade.php
+++ app/views/admin/users.blade.php
@@ -32,7 +32,7 @@
         <td class="td-inp"><label><input type="text" ng-model="user.first_name" ng-blur="save(u, user)" ng-change="user.dirty=1"></label></td>
         <td class="td-inp"><label><input type="text" ng-model="user.last_name" ng-blur="save(u, user)" ng-change="user.dirty=1"></label></td>
         <td class="td-inp"><label><input type="email" ng-model="user.email" ng-blur="save(u, user)" ng-change="user.dirty=1"></label></td>
-        <td ng-click="overlay=1">Kalenders...</td>
+        <td ng-click="open(user)">Kalenders...</td>
       </tr>
     </tbody>
   </table>
@@ -40,21 +40,35 @@
   <p ng-hide="(users | filter:search).length">
     Geen resultaten
   </p>
+  <div id="overlay" ng-show="overlay" ng-click="overlay=0" class="ng-cloak">
+    <div class="modal-container">
+      <div class="modal-body" ng-click="$event.stopPropagation();">
+        <div class="close" ng-click="overlay=0">&times;</div>
+        <h1 ng-bind="user.first_name+' '+user.last_name" ng-click="overlay=0" class="modal-title"></h1>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Toegang</th>
+              <th>Kalender</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr ng-repeat="(c, cal) in cals | filter:search" ng-class="{saved:user.saved,error:user.error,dirty:user.dirty}">
+              <td class="td-checkbox" ng-class="{'td-checkbox--active':user.activated}"><label><input type="checkbox" name="name" ng-model="user.activated"></label></td>
+              <td class="td-inp"><label><input type="text" ng-model="user.first_name" ng-blur="save(u, user)" ng-change="user.dirty=1"></label></td>
+              <td class="td-inp"><label><input type="text" ng-model="user.last_name" ng-blur="save(u, user)" ng-change="user.dirty=1"></label></td>
+              <td class="td-inp"><label><input type="email" ng-model="user.email" ng-blur="save(u, user)" ng-change="user.dirty=1"></label></td>
+              <td ng-click="open(user)">Kalenders...</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </form>
 @stop
 
 @section('footerScript')
-<div id="overlay" ng-show="overlay" ng-click="overlay=0" class="ng-cloak">
-  <div class="modal-container">
-    <div class="modal-body" ng-click="$event.stopPropagation();">
-      <div class="close" ng-click="overlay=0">&times;</div>
-      <h1 ng-bind="user.first_name+' '+user.last_name" ng-click="overlay=0"></h1>
-      <p>
-        List of calendars
-      </p>
-    </div>
-  </div>
-</div>
 {{ HTML::script('bower_components/angular/angular.min.js') }}
 {{ HTML::script('bower_components/angular-resource/angular-resource.min.js') }}
 <script type="text/javascript">
@@ -69,6 +83,12 @@ angular.module('users', ['ngResource'])
         id: '@id'
       });
     $scope.users = users.query();
+    $scope.overlay = 0;
+
+    $scope.open = function(user) {
+      $scope.user = user;
+      $scope.overlay = 1;
+    }
 
     $scope.save = function(u, user) {
       if (!user.dirty) return;
