0da8912eceb2291fb7f4fdead00f4105acbab277
Bertware <bert.marcelis@outlook.com>
2015-07-31 13:27:31 +0200

    Improved password forgotten mail

--- app/views/emails/passwordreset.blade.php
+++ app/views/emails/passwordreset.blade.php
@@ -5,9 +5,18 @@
 </head>
 <body>
 <p>
-    Iemand heeft een wachtwoord reset aangevraagd via de website. Indien je je wachtwoord wilt wijzigen, kan dat via
-    deze link: <a href="{{$url}}">{{$url}}</a>
-    Indien je dit niet aangevraagd hebt kan je deze mail negeren, en zal je huidig wachtwoord behouden blijven.
+
+    Beste @if (isset($user) && isset($user->first_name)) {{$user->first_name}}@endif,<br><br>
+
+    Je gaf aan je wachtwoord vergeten te zijn. Via onderstaande link kan je je wachtwoord herstellen.<br>
+
+    Volg volgende link om je wachtwoord opnieuw in te stellen. Indien de link niet werkt, kopieer en plak hem dan in de
+    adresbalk van je browser.<br>
+    <a href="{{$url}}">{{$url}}</a><br><br>
+    Alvast veel plezier bij het plannen!<br>
+    Het educal team<br><br><br><br>
+    Heb je dit niet aangevraagd? Negeer deze mail dan.<br>
+    Bij verdere vragen of problemen, bezoek <a href="http://educal.gent.be/">educal.gent.be</a>.
 </p>
 </body>
 </html>
